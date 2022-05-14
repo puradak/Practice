f0349003056d94fbb5a7b8e06406cf0c7624b6ae
puradak <bbln2252@naver.com>
2022-05-15 00:28:58 +0900

    무한 반복되게끔 코드 변경ver

--- _22_05_14_/BRN_TESTER.java
+++ _22_05_14_/BRN_TESTER.java
@@ -1,10 +1,15 @@
 package _22_05_14_;
 
+import java.util.Scanner;
+
 public class BRN_TESTER {
 	public static void main(String args[]) {
 		BigRealNumber f = new BigRealNumber();
-		f.factorial(100);
-		f.print();
-		return;
+		Scanner input = new Scanner(System.in);
+		while(true) {
+			System.out.print("정수를 입력 : ");
+			f.factorial(input.nextInt());
+			f.print();	
+		}
 	}
 }
